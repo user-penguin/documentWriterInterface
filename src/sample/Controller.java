16b524b4b49472e21be6c4e980f2e92eda6a10b4
user-penguin <kobzev96@gmail.com>
2019-04-17 05:42:01 +0700

    create calculate II chetvert and method convertVoid

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField itogIProdajiRub, itogIProdajiKop,
+            itogIProizvodRub, itogIProizvodKop,
+            summaZaObshieProdRub, summaZaObshieProdKop,
+            summaZaPorcProdRub, summaZaPorcProdKop,
+            summaZaObediProdRub, summaZaObediProdKop;
+
+    @FXML
+    public void initialize () {
+
+    }
+
+    @FXML
+    public void update () {
+        updateIIchetvert();
+    }
+
+    private void updateIIchetvert () {
+        int allKop = convertVoid(summaZaObediProdKop) + convertVoid(summaZaPorcProdKop) + convertVoid(summaZaObshieProdKop);
+        int realKop = allKop % 100;
+        int ostKop = allKop / 100;
+        int rub = convertVoid(summaZaObediProdRub) + convertVoid(summaZaPorcProdRub) +
+                convertVoid(summaZaObshieProdRub) + ostKop;
+        itogIProdajiRub.setText(String.valueOf(rub));
+        itogIProdajiKop.setText(String.valueOf(realKop));
+    }
+
+    private int convertVoid (TextField textField) {
+        if (textField.getText().equals("")) {
+            return 0;
+        } else {
+            return Integer.parseInt(textField.getText());
+        }
+    }
 }
